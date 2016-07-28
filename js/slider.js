a46c666f5d4eeb62582a92b6a13b2bd74e80c053
inaki <iiaranzadi@gmail.com>
2016-07-28 15:02:49 -0700

    first commit

---
+++ js/slider.js
@@ -0,0 +1,15 @@
+function createSlider() {
+	var th = $( this );
+		th.sldr({
+			focalClass    : 'focalPoint',
+			offset        : th.width() / 2,
+			sldrWidth     : 'responsive',
+			nextSlide     : th.nextAll( '.sldr-nav.next:first' ),
+			previousSlide : th.nextAll( '.sldr-nav.prev:first' ),
+			selectors     : th.nextAll( '.selectors:first' ).find( 'li' ),
+			toggle        : th.nextAll( '.captions:first' ).find( 'div' ),
+			sldrAuto      : true,
+			sldrTime      : 8000,
+			hasChange     : true
+	});
+}
